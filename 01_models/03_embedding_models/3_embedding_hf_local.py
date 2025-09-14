9317b0ef6da10fbd96c2fd022996200e67543894
dev-rakesshaw <rakesh.shaw018@gmail.com>
2025-09-14 17:32:18 +0530

    need to work on hugging face codes

---
+++ 01_models/03_embedding_models/3_embedding_hf_local.py
@@ -0,0 +1,36 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+#Local Enbedding
+# embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+# documents = [
+#     "Delhi is the capital of India",
+#     "Kolkata is the capital of West Bengal",
+#     "Paris is the capital of France"
+# ]
+
+# vector = embedding.embed_documents(documents)
+
+# print(str(vector))
+
+
+#Using inference API
+import os
+from huggingface_hub import InferenceClient
+
+load_dotenv()
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=os.environ["HUGGINGFACEHUB_API_TOKEN"],
+)
+
+result = client.sentence_similarity("That is a happy person",
+    other_sentences=[
+        "That is a happy dog",
+        "That is a very happy person",
+        "Today is a sunny day"
+    ],
+    model="sentence-transformers/all-MiniLM-L6-v2",
+)
